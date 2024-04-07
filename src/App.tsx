8377d8289fa2d69fb4d2a0cc2a9c0e5563bc7392
Anish Sah <anishsah7@gmail.com>
2024-04-07 19:52:32 +0545

    test branch

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,5 @@
-import { HashRouter, Routes, Route } from 'react-router-dom'
-import {
-  AboutPage,
-  ContactPage,
-  LandingPage,
-  PortfolioPage,
-  TeamPage,
-} from './pages'
-import ErrorPage from './pages/ErrorPage'
-import { Footer, Navbar, NextButton, ScrollToTop } from './components'
-
 function App() {
-  return (
-    <HashRouter>
-      <ScrollToTop />
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="/get-in-touch" element={<ContactPage />} />
-        <Route path="/portfolio" element={<PortfolioPage />} />
-        <Route path="/team" element={<TeamPage />} />
-        <Route path="*" element={<ErrorPage />} />
-      </Routes>
-      <NextButton />
-      <Footer />
-    </HashRouter>
-  )
+  return <main>Grid Design Consultancy and Engineering Service Pvt. Ltd.</main>
 }
 
 export default App
