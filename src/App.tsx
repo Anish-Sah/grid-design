0adac14ada2af0cbfddc4b24a6b0608effa7d17c
Anish Sah <anishsah7@gmail.com>
2024-04-07 23:10:44 +0545

    fix: app page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,33 @@
+import { HashRouter, Routes, Route } from 'react-router-dom'
+import {
+  AboutPage,
+  ContactPage,
+  LandingPage,
+  PortfolioPage,
+  TeamPage,
+} from './pages'
+import ErrorPage from './pages/ErrorPage'
+import { Footer, Navbar, NextButton, ScrollToTop } from './components'
+
+// HashRouter is used to create a hash-based routing system, because GitHub Pages doesn't support browser history routing.
+
 function App() {
-  return <main>Grid Design Consultancy and Engineering Service Pvt. Ltd.</main>
+  return (
+    <HashRouter>
+      <ScrollToTop />
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/get-in-touch" element={<ContactPage />} />
+        <Route path="/portfolio" element={<PortfolioPage />} />
+        <Route path="/team" element={<TeamPage />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+      <NextButton />
+      <Footer />
+    </HashRouter>
+  )
 }
 
 export default App
